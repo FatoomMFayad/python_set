b72a0ebd526fe01d16421ff3212dec2f62db00e6
ffayad <ffayad@ptcdb.edu.ps>
2025-01-02 14:03:37 +0200

    word occurrences in a text using dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,16 @@
+from enum import unique
+
 country_codes = {'PS':'970', 'Canada':'1', 'Turkey':'90'}
 
 print(country_codes['Turkey'])
 
 country_codes.update({'Jordan':'962', 'Qatar': '974'})
 print(country_codes)
+
+text = "My name is Fatoom, I live in Gaza . Gaza is a nice city in Palestine. Palestine is a nice country in Middle East"
+tokens = text.split(' ')
+unique_tokens = set(tokens)
+dict = {}
+for token in unique_tokens:
+    dict[token] = tokens.count(token)
+print(dict)
