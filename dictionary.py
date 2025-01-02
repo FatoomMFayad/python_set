f913b29073cfb4dc0c8b5be52ce79f081caad040
ffayad <ffayad@ptcdb.edu.ps>
2025-01-02 14:35:12 +0200

    comment

--- dictionary.py
+++ dictionary.py
@@ -6,7 +6,7 @@ print(country_codes['Turkey'])
 
 country_codes.update({'Jordan':'962', 'Qatar': '974'})
 print(country_codes)
-
+#unique 
 text = "My name is Fatoom, I live in Gaza . Gaza is a nice city in Palestine. Palestine is a nice country in Middle East"
 tokens = text.split(' ')
 unique_tokens = set(tokens)
