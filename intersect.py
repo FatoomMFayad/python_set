ad1bf2d81672f21c7b75ca6fed7f6d75fc988fb5
ffayad <ffayad@ptcdb.edu.ps>
2025-01-02 13:43:22 +0200

    difference

--- intersect.py
+++ intersect.py
@@ -1,3 +1,6 @@
 names_1 = ['Fatoom', 'Ahmed', 'Mohammed', 'Omar']
 names_2 = ['Ahmed', 'Omar']
+#intersection
 print(set(names_1).intersection(names_2))
+#difference
+print(set(names_1).difference(set(names_2)))
